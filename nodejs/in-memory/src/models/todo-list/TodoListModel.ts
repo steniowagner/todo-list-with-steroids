c1d56239f73ba6b03153dad47fe7b71af45d1b21
Stenio Wagner <stenio.wagner1@gmail.com>
2022-12-27 12:53:21 -0300

    [nodejs][in-memory] feat: craeting the TodoListModel

---
+++ nodejs/in-memory/src/models/todo-list/TodoListModel.ts
@@ -0,0 +1,26 @@
+import { Todo } from "../Todo";
+
+export class TodoListModel {
+  public todos: Todo[];
+
+  constructor() {
+    this.todos = [];
+  }
+
+  add = (todo: Todo) => this.todos.push(todo);
+
+  readAll = () => this.todos;
+
+  readById = (id: string) => this.todos.find((todo) => todo.id === id);
+
+  update = (updatedTodo: Todo) => {
+    const updatedTodoIndex = this.todos.findIndex(
+      (todo) => todo.id === updatedTodo.id
+    );
+    this.todos[updatedTodoIndex] = updatedTodo;
+  };
+
+  delete = (id: string) => {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  };
+}
