2a2b64a79839aa76688645e269aa1e998e496d61
Stenio Wagner <stenio.wagner1@gmail.com>
2022-12-29 23:32:52 -0300

    [nodejs][in-memory] feat: creating the views/TodoMenuView

---
+++ nodejs/in-memory/src/views/todo/TodoMenu.ts
@@ -0,0 +1,13 @@
+export const EXIT_OPTION = "6";
+
+export class TodoMenuView {
+  static main = () => `
+1 -- Create todo
+2 -- Read all todos
+3 -- Read one todo (by id)
+4 -- Edit a todo
+4 -- Mark a todo as "completed"
+5 -- Remove a todo
+${EXIT_OPTION} -- Exit
+`;
+}
