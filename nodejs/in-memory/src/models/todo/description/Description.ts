f881ea9369634d3e284e14b56d2efdefc978c61c
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-02 12:41:01 -0300

    [nodejs][in-memory] feat: refactoring the Todo/Description

--- nodejs/in-memory/src/models/todo/description/Description.ts
+++ nodejs/in-memory/src/models/todo/description/Description.ts
@@ -4,26 +4,18 @@ export const MAX_CHARACTERS = 255;
 export const MIN_CHARATERS = 2;
 
 export class Description {
-  private readonly description: string;
-
-  private constructor(description: string) {
-    this.description = description;
-    Object.freeze(this);
-  }
+  private constructor(private description: string) {}
 
   get value() {
     return this.description;
   }
 
-  static validate(description: string) {
-    return (
-      !!description &&
-      description.trim().length > MIN_CHARATERS &&
-      description.trim().length <= MAX_CHARACTERS
-    );
+  set value(description: string) {
+    Description.handleValidate(description);
+    this.description = description;
   }
 
-  static create(description: string) {
+  private static handleValidate(description: string) {
     if (!Description.validate(description)) {
       throw new DescriptionErrors({
         name: "INVALID_DESCRIPTION",
@@ -31,6 +23,18 @@ export class Description {
         cause: undefined,
       });
     }
+  }
+
+  static validate(description: string) {
+    return (
+      !!description &&
+      description.trim().length >= MIN_CHARATERS &&
+      description.trim().length <= MAX_CHARACTERS
+    );
+  }
+
+  static create(description: string) {
+    this.handleValidate(description);
     return new Description(description);
   }
 }
