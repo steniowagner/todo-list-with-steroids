63dcbe6f09076295e84c260732a8ef1a73afd083
Stenio Wagner <stenio.wagner1@gmail.com>
2023-01-07 22:26:09 -0300

    [nodejs][in-memory] feat: removing the Priority and Flag from the ParseTodoToString

--- nodejs/in-memory/src/models/todo/utils/parse-todo-to-string/ParseTodoToString.test.ts
+++ nodejs/in-memory/src/models/todo/utils/parse-todo-to-string/ParseTodoToString.test.ts
@@ -1,9 +1,6 @@
 import { ParseTodoToString } from "./ParseTodoToString";
-import { Priority } from "../../Priority";
 import { Status } from "../../Status";
 
-const priorities = [Priority.LOW, Priority.MEDIUM, Priority.HIGH];
-const prioritiesValues = ["Low", "Medium", "High"];
 const statuses = [Status.TODO, Status.DOING, Status.DONE];
 const statusesValues = ["Todo", "Doing", "Done"];
 const baseParams = {
@@ -20,7 +17,7 @@ describe("Todo/ParseTodoToString", () => {
           isFinished: true,
         })
       ).toEqual(
-        `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? Yes\n- Priority: Unknown\n- Status: Unknown\n- Flag: -`
+        `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? Yes\n- Status: Unknown`
       );
     });
 
@@ -31,24 +28,8 @@ describe("Todo/ParseTodoToString", () => {
           isFinished: false,
         })
       ).toEqual(
-        `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? No\n- Priority: Unknown\n- Status: Unknown\n- Flag: -`
+        `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? No\n- Status: Unknown`
       );
-    });
-  });
-
-  describe('When the "Priority" is defined', () => {
-    it("should parse correctly", () => {
-      priorities.forEach((priority, index) => {
-        expect(
-          ParseTodoToString.parse({
-            ...baseParams,
-            isFinished: false,
-            priority,
-          })
-        ).toEqual(
-          `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? No\n- Priority: ${prioritiesValues[index]}\n- Status: Unknown\n- Flag: -`
-        );
-      });
     });
   });
 
@@ -62,43 +43,9 @@ describe("Todo/ParseTodoToString", () => {
             status,
           })
         ).toEqual(
-          `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? No\n- Priority: Unknown\n- Status: ${statusesValues[index]}\n- Flag: -`
+          `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? No\n- Status: ${statusesValues[index]}`
         );
       });
-    });
-  });
-
-  describe('When both "Status" and "Priorty" are defined', () => {
-    it("should parse correctly", () => {
-      for (let i = 0; i < statuses.length; i++) {
-        for (let j = 0; j < priorities.length; j++) {
-          expect(
-            ParseTodoToString.parse({
-              ...baseParams,
-              isFinished: false,
-              status: statuses[i],
-              priority: priorities[j],
-            })
-          ).toEqual(
-            `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? No\n- Priority: ${prioritiesValues[j]}\n- Status: ${statusesValues[i]}\n- Flag: -`
-          );
-        }
-      }
-    });
-  });
-
-  describe('When the "Todo" is "Flagged"', () => {
-    it('should show the "Flag" correctly', () => {
-      const flag = "SOME_FLAG";
-      expect(
-        ParseTodoToString.parse({
-          ...baseParams,
-          flag,
-          isFinished: true,
-        })
-      ).toEqual(
-        `- id: ${baseParams.id}\n- Description: ${baseParams.description}\n- Finished? Yes\n- Priority: Unknown\n- Status: Unknown\n- Flag: ${flag}`
-      );
     });
   });
 });
--- nodejs/in-memory/src/models/todo/utils/parse-todo-to-string/ParseTodoToString.ts
+++ nodejs/in-memory/src/models/todo/utils/parse-todo-to-string/ParseTodoToString.ts
@@ -1,29 +1,14 @@
-import { Priority } from "../../Priority";
 import { Status } from "../../Status";
 import { TodoDTO } from "../../TodoDTO";
 
 type ParseTodoToStringParams = TodoDTO & {
   isFinished: boolean;
-  flag?: string;
   id: string;
 };
 
 export class ParseTodoToString {
   private static parseIsFinished(isFinished: boolean) {
     return isFinished ? "Yes" : "No";
-  }
-
-  private static parsePriority(priority?: Priority) {
-    switch (priority) {
-      case Priority.HIGH:
-        return "High";
-      case Priority.MEDIUM:
-        return "Medium";
-      case Priority.LOW:
-        return "Low";
-      default:
-        return "Unknown";
-    }
   }
 
   private static parseStatus(status?: Status) {
@@ -39,19 +24,11 @@ export class ParseTodoToString {
     }
   }
 
-  private static parseFlag(flag?: string) {
-    return flag || "-";
-  }
-
   static parse(params: ParseTodoToStringParams) {
     return `- id: ${params.id}\n- Description: ${
       params.description
     }\n- Finished? ${this.parseIsFinished(
       params.isFinished
-    )}\n- Priority: ${this.parsePriority(
-      params.priority
-    )}\n- Status: ${this.parseStatus(params.status)}\n- Flag: ${this.parseFlag(
-      params.flag
-    )}`;
+    )}\n- Status: ${this.parseStatus(params.status)}`;
   }
 }
